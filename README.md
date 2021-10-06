e393520f98ef771f134cbca3fa9541cff7affc22
tannerlsmith <tanner.smith@uvm.edu>
2021-10-06 09:58:03 -0400

    handling form submission

--- README.md
+++ README.md
@@ -9,3 +9,29 @@
 * JS sets fetch() requests aside and continues implemented the rest of your code, until the fetch callback is ready. This is asynchronous code. 
 * Async code is referred to as AJAX. 
 * With a response in place, look at the response object that is logged. 
+* Fetch request looks like:
+<br>
+![fetch-request](./assets/images/fetch-request.png)
+
+### Adding the Search Form
+* The form with input capability will look like:
+<br>
+![form input for html](./assets/images/form-input.png)
+
+* Now handling the form submission by making it functional:
+* We need to create variables to store references to the form: 
+<br>
+
+* ```var userFormEl = document.querySelector("#user-form");```
+<br>
+
+* ```var nameInputEl = document.querySelector("#username");```
+<br>
+
+* Next, create a handler event: 
+<br>
+
+* ![alt](./assets/images/form-handler.png)
+<br>
+
+* with ```userFormEl.addEventListener('submit', formSubmitHandler);``` in place, check functionality by clicking the button and looking at the console. 
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,33 @@
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    });
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert('Please Enter a GitHub username');
+  }
+  console.log(event);
 };
 
-getUserRepos('tannerlsmith')
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+getUserRepos("tannerlsmith");
